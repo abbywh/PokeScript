f1d7ae64c1256ab45a8da03a39ca17470a364dce
Joel Whittier <jdw23@uchicago.edu>
2020-12-13 19:10:19 -0500

    pure attack component, creating commands

--- build/src/Pokemon.hs
+++ build/src/Pokemon.hs
@@ -4,6 +4,7 @@ type PokeName = String
 ---Will eventually become a or Constructor
 type Ability = String
 type Type = String
+type Nature = String
 
 data Attack = Attack {aType:: Type, pp:: Integer, basePower::Integer,
   isSp :: Bool, acc:: Integer} deriving (Show)
@@ -11,4 +12,19 @@ data Attack = Attack {aType:: Type, pp:: Integer, basePower::Integer,
 data Pokemon = Pokemon {name:: PokeName, ability1 :: Ability,
   ability2 :: Ability, type1:: Type, type2:: Type, hp:: Integer, atk:: Integer,
   def:: Integer, spAtk :: Integer, spDef :: Integer, speed :: Integer,
-  moves:: [Attack]} deriving (Show)
+  moves:: [Attack], isBurned :: Bool, nat :: Nature, evs ::[Integer]} deriving (Show)
+
+--attack with only non RNG components
+--pureAttack :: Pokemon -> Attack -> Pokemon -> Integer
+pureAttack p1 at p2= (((((2*level) `div` 5 + 2) * power * at1 `div` d1)
+  `div` 50)+2)*modify
+  where
+    power = basePower at
+    level = 100
+    stab = if aType at == type1 p1 || aType at == type2 p1 then 1.5 else 1
+    --need to implement types
+    tp = 1
+    burn = if isBurned p1 then 1 `div` 2 else 1
+    at1 = atk p1
+    d1 = def p2
+    modify = tp * burn
--- build/src/Test.hs
+++ build/src/Test.hs
@@ -6,10 +6,13 @@ import Pokemon
 constructTests = []
 
 
+
 eq = Attack {aType="Ground", pp=12, basePower = 100, isSp = False,
   acc = 100}
 
 
 groudon = Pokemon {name= "Groudon", ability1="Drought", ability2="None",
   type1="Ground", type2= "", hp = 100, atk=150, def= 140, spAtk=100, spDef=90,
-  speed = 90 , moves=[eq]}
+  speed = 90 , isBurned=False, nat="", moves=[eq], evs = [0,0,0,0,0,0]}
+
+--test1 = TestCase (assertEqual ("",))
